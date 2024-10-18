9e8628a3f72d89aa96dbe56fb53456eb26aec10d
Wietse de Vries <wietsedv@proton.me>
2024-10-18 10:26:21 +0200

    add venue info

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SHORT_TITLE = 'AQL 2025';
-export const FULL_TITLE = '16th International Conference on Advances in Quantitative Laryngology, Voice and Speech Research (AQL)';
+export const FULL_TITLE = 'The 16th International Conference on Advances in Quantitative Laryngology, Voice and Speech Research (AQL)';

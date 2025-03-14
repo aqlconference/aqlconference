a70fd1d04c3f81877cac331b3a9e1ee8cf5ecbac
Valentine Lucquiault <74610244+v-lucquiault@users.noreply.github.com>
2025-03-14 14:37:27 +0100

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './build/js/*.js'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 		container: {
 			center: true,
-		},
-		keyframes: {
-		        'open-menu': {
-		          '0%': { transform: 'scaleY(0)' },
-		          '80%': { transform: 'scaleY(1.2)' },
-		          '100%': { transform: 'scaleY(1)' },
-		        },
-		      },
-		      animation: {
-		        'open-menu': 'open-menu 0.5s ease-in-out forwards',
-		      }
 		},
 	plugins: [
 		require('@tailwindcss/typography'),

b269eb9f56c1f69bc06c8dead3d9a00dcc9719f8
yousefahmedm <ahmedmiyousef@gmail.com>
2026-07-24 00:32:38 -0400

    Update conference titles for AQL 2027 in consts.ts⁠

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SHORT_TITLE = 'AQL 2025';
-export const FULL_TITLE = 'The 16th International Conference on Advances in Quantitative Laryngology, Voice and Speech Research (AQL)';
+export const SHORT_TITLE = 'AQL 2027';
+export const FULL_TITLE = 'The 17th International Conference on Advances in Quantitative Laryngology, Voice and Speech Research (AQL 2027)';
